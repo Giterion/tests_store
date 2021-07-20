c0f3d4051865f12131038736e83b4d42855f4bc6
Giterion <ltmsk@mail.ru>
2021-07-20 16:32:28 +0300

    Added ProductPageLocators in locators.py

--- pages/locators.py
+++ pages/locators.py
@@ -4,3 +4,6 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     BOOK_NAME_IN_MESSAGE_ABOUT_ADD = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
+
+class ProductPageLocators():
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
