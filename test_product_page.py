31fee3167f35cbbe44f0aedb44e663d5b483b484
Giterion <ltmsk@mail.ru>
2021-08-16 17:57:32 +0300

    In test_product_page.py added def test_guest_cant_see_success_message_after_adding_product_to_basket(browser); added def test_guest_cant_see_success_message(browser); added def test_message_disappeared_after_adding_product_to_basket

--- test_product_page.py
+++ test_product_page.py
@@ -1,3 +1,4 @@
+from pages.locators import ProductPageLocators
 from .pages.main_page import MainPage
 from .pages.base_page import BasePage
 import time
@@ -16,10 +17,43 @@ import pytest
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 
 
-def test_guest_can_add_product_to_basket(browser, link):
+# def test_guest_can_add_product_to_basket(browser, link):
+#     page = MainPage(browser, link)
+#     page.open()
+#     page.can_add_product_to_basket()
+#     page.solve_quiz_and_get_code()
+#     time.sleep(1)
+#     page.correctly_book_name_in_allert_about_add_to_basket()
+
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = MainPage(browser, link)
     page.open()
+    time.sleep(3)
     page.can_add_product_to_basket()
-    page.solve_quiz_and_get_code()
-    time.sleep(1)
-    page.correctly_book_name_in_allert_about_add_to_basket()
+    time.sleep(3)
+    assert page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE)
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = MainPage(browser, link)
+    page.open()
+    time.sleep(3)
+    assert page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE)
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = MainPage(browser, link)
+    page.open()
+    time.sleep(3)
+    page.can_add_product_to_basket()
+    time.sleep(3)
+    assert page.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE)
+
+
+
+
+
+
+
+
