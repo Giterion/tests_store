42fcdda59935af7a988fa2693c1d96b4e74f1db3
Giterion <ltmsk@mail.ru>
2021-08-19 11:48:34 +0300

    In locators.py added class LoginPageLocators and class ProductPageLocators.

--- pages/locators.py
+++ pages/locators.py
@@ -5,5 +5,17 @@ class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     BOOK_NAME_IN_MESSAGE_ABOUT_ADD = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
 
+class LoginPageLocators:
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
 class ProductPageLocators():
-    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages > div.alert.alert-safe.alert-noicon.alert-success.fade.in")
+    MESSAGE_PRODUCT_IN_CART = (By.CSS_SELECTOR, "#messages > div:nth-child(1)")
+    ADD_TO_CART = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.col-sm-6.product_main>h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".price_color")
+    ALERT_PRODUCT_NAME = (By.CSS_SELECTOR,"div.alertinner>strong")
+
+
+
