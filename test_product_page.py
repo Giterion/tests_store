749e77311c0f85bab3437b8ffbc050919dfbd7b9
Giterion <ltmsk@mail.ru>
2021-08-25 23:05:48 +0300

    in test_product_page.py upgraded imports; fixed problem with start tests from terminal; added mark.skip and mark.xfail

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,6 @@
+from pages.main_page import MainPage
 from pages.locators import ProductPageLocators
-from .pages.main_page import MainPage
-from .pages.base_page import BasePage
+
 import time
 import pytest
 
@@ -16,39 +16,39 @@ import pytest
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 
+@pytest.mark.skip
+def test_guest_can_add_product_to_basket(browser, link):
+    page = MainPage(browser, link)
+    page.open()
+    page.can_add_product_to_basket()
+    page.solve_quiz_and_get_code()
+    time.sleep(1)
+    page.correctly_book_name_in_allert_about_add_to_basket()
 
-# def test_guest_can_add_product_to_basket(browser, link):
-#     page = MainPage(browser, link)
-#     page.open()
-#     page.can_add_product_to_basket()
-#     page.solve_quiz_and_get_code()
-#     time.sleep(1)
-#     page.correctly_book_name_in_allert_about_add_to_basket()
-
+@pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = MainPage(browser, link)
     page.open()
-    time.sleep(3)
     page.can_add_product_to_basket()
-    time.sleep(3)
-    assert page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE)
+    assert page.is_not_element_present(*ProductPageLocators.MESSAGE_PRODUCT_IN_CART)
+
 
 def test_guest_cant_see_success_message(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = MainPage(browser, link)
     page.open()
     time.sleep(3)
-    assert page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE)
+    assert page.is_not_element_present(*ProductPageLocators.MESSAGE_PRODUCT_IN_CART)
 
+@pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = MainPage(browser, link)
     page.open()
-    time.sleep(3)
     page.can_add_product_to_basket()
-    time.sleep(3)
-    assert page.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE)
+    assert page.is_disappeared(*ProductPageLocators.MESSAGE_PRODUCT_IN_CART)
+
 
 
 
