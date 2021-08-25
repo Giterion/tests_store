8b30fae20b77868f5193b9407fed3a7e3f7eb4d4
Giterion <ltmsk@mail.ru>
2021-08-25 22:59:00 +0300

    in locators.py upgraded selectors

--- pages/locators.py
+++ pages/locators.py
@@ -1,11 +1,12 @@
 from selenium.webdriver.common.by import By
 
 
+
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     BOOK_NAME_IN_MESSAGE_ABOUT_ADD = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
 
-class LoginPageLocators:
+class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
 
