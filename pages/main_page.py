b971204d11149ae0a49fb22ec2ec938e77429db1
Giterion <ltmsk@mail.ru>
2021-08-25 22:59:58 +0300

    in main_page.py upgraded imports

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,9 @@
-from .base_page import BasePage
+# from .base_page import BasePage
+# from selenium.webdriver.common.by import By
 from selenium.webdriver.common.by import By
-from selenium import webdriver
-from .locators import MainPageLocators
+from pages.base_page import BasePage
+from .base_page import BasePage
+
 
 class MainPage(BasePage):
     def go_to_login_page(self):
