b6ac61fe23589ab7bc632bff687ae484a34198ff
Giterion <ltmsk@mail.ru>
2021-10-08 15:21:15 +0300

    in product_page.py added def go_to_basket and added import By

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,5 +1,6 @@
 from .base_page import BasePage
 from .locators import ProductPageLocators
+from selenium.webdriver.common.by import By
 
 
 class ProductPage(BasePage):
@@ -12,9 +13,11 @@ class ProductPage(BasePage):
 
     def should_add_to_cart_page(self):
         assert self.is_element_present(*ProductPageLocators.ADD_TO_CART), "Add btn is not presented"
+
     def product_name(self):
         name=self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
         return name.text
+
     def product_price(self):
         price=self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
         return price.text
@@ -26,3 +29,10 @@ class ProductPage(BasePage):
     def should_disappeared(self):
         assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
             "Success message is not disappeared"
+
+
+    def go_to_basket(self):
+        go_to_basket_button = self.browser.find_element(By.XPATH, '//*[@id="default"]/header/div[1]/div/div[2]/span/a')
+        go_to_basket_button.click()
+
+
