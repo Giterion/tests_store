ec420160383b5ec72c4afc793bd6eca3b9967f83
Giterion <ltmsk@mail.ru>
2021-10-29 17:44:47 +0300

    in test_product_page.py added class TestUserAddToBasketFromProductPage with test_user_cant_see_success_message and test_user_can_add_product_to_basket

--- test_product_page.py
+++ test_product_page.py
@@ -2,7 +2,8 @@ from pages.main_page import MainPage
 from pages.locators import ProductPageLocators
 from pages.locators import MainPageLocators
 from pages.product_page import ProductPage
-
+from pages .base_page import BasePage
+from pages .login_page import LoginPage
 
 import time
 import pytest
@@ -59,6 +60,38 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page.go_to_basket()
     assert page.is_not_element_present(*ProductPageLocators.PRODUCT_NAME)
     assert page.is_element_present(*MainPageLocators.YOUR_CART_IS_EMPTY)
+
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
+        page = LoginPage(browser, link)
+        page.open()
+        #time.sleep(2)
+        email = str(time.time()) + '@fakemail.org'
+        password = str(time.time()) + 'S2312124ss3212d'
+        page.register_new_user(email, password)
+        #time.sleep(2)
+        page.should_be_authorized_user()
+        #time.sleep(2)
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        page = MainPage(browser, link)
+        page.open()
+        #time.sleep(1)
+        assert page.is_not_element_present(*ProductPageLocators.MESSAGE_PRODUCT_IN_CART)
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
+        page = MainPage(browser, link)
+        page.open()
+        page.can_add_product_to_basket()
+       # page.solve_quiz_and_get_code()
+        #time.sleep(1)
+        page.correctly_book_name_in_allert_about_add_to_basket()
+
+
 
 
 
