eddabff7a87af7e02a43567acedab76f495bb7be
Giterion <ltmsk@mail.ru>
2021-10-29 17:50:57 +0300

    in LoginPage added function register_new_user

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,7 @@
+import time
 from .base_page import BasePage
 from .locators import LoginPageLocators
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
@@ -21,3 +23,23 @@ class LoginPage(BasePage):
 
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+
+    def register_new_user(self, email, password):
+        email_field = self.browser.find_element(*LoginPageLocators.EMAIL_FIELD)
+        email_field.send_keys(email)
+        password_field = self.browser.find_element(*LoginPageLocators.PASSWORD_FIELD)
+        password_field.send_keys(password)
+        confirm_password_field = self.browser.find_element(*LoginPageLocators.CONFIRM_PASSWORD_FIELD)
+        confirm_password_field.send_keys(password)
+        registration_confirm = self.browser.find_element(*LoginPageLocators.REGISTRATION_CONFIRM_BUTTON)
+        registration_confirm.click()
+
+
+
+
+
+
+
+
+
+
