6552de789f9c12de3ed0b00a4e55456e49c5bb63
Giterion <ltmsk@mail.ru>
2021-11-01 14:06:08 +0300

    added basket_should_be_empty function

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -1,8 +1,12 @@
 from .base_page import BasePage
 from .locators import BasePageLocators
+from .locators import BasketPageLocators
 
 
 class BasketPage(BasePage):
     def should_not_bee_basket_link_in_basket(self):
         assert self.is_not_element_present(*BasePageLocators.BASKET_LINK)
+
+    def basket_should_be_empty(self):
+        assert self.is_element_present(*BasketPageLocators.BASKET_EMPTY)
 
