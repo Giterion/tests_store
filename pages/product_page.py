ab0e6f7c3f43ec9f4eab5c4fe0462a21b7a2a644
Giterion <ltmsk@mail.ru>
2021-11-03 15:19:50 +0300

    removed the commented out code

--- pages/product_page.py
+++ pages/product_page.py
@@ -9,9 +9,6 @@ class ProductPage(BasePage):
     def add_to_cart_page(self):
         add_btn = self.browser.find_element(*ProductPageLocators.ADD_TO_CART)
         add_btn.click()
-        #self.solve_quiz_and_get_code()
-        #alert_name_product=self.browser.find_element(*ProductPageLocators.ALERT_PRODUCT_NAME)
-        #return alert_name_product.text
 
     def should_add_to_cart_page(self):
         assert self.is_element_present(*ProductPageLocators.ADD_TO_CART), "Add btn is not presented"
