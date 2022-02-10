fb77451f3f8e513e374bbb93ea22a64e4dd690a3
Giterion <ltmsk@mail.ru>
2022-02-10 16:10:05 +0300

    added mark.main_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,8 @@
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
+import pytest
 
-
-
+@pytest.mark.main_page
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
@@ -11,18 +11,21 @@ def test_guest_can_go_to_login_page(browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
+@pytest.mark.main_page
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.main_page
 def test_guest_should_see_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page2 = LoginPage(browser, link)
     page2.open()
     page2.should_be_login_page()
 
+@pytest.mark.main_page
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
